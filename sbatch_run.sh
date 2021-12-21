1118c6a9a42f45417cf93e3a398024112b75886f
rajarshd <rajarshi@cs.umass.edu>
2021-12-21 13:08:41 -0800

    checking in for distributed runs

--- sbatch_run.sh
+++ sbatch_run.sh
@@ -3,9 +3,9 @@
 #SBATCH --output=logs/obl-per-relation-%A_%a.out
 #SBATCH --partition=longq
 #SBATCH --time=07-00:00:00
-#SBATCH --mem=80G
-#SBATCH --array=0-29
-#SBATCH --exclude=swarm[001-021,025-028,030,032-038,068-072,076,104-105,022-024,051,103,052-055,031,029]
+#SBATCH --mem=40G
+#SBATCH --array=0-1
+#SBATCH --exclude=swarm[001-009,010-038,068-090,104-105,051-055]
 # Set to scratch/work since server syncing will occur from here
 # Ensure sufficient space else runs crash without error message
 
@@ -16,7 +16,7 @@
 #conda activate obl
 #wandb agent rajarshd/Open-BIo-Link-src_prob_cbr/9neli3pq
 
-#source /mnt/nfs/work1/pthomas/agodbole/Open-BIo-Link/wandb_settings.sh
+#source /mnt/nfs/work1/mccallum/rajarshi/Dropbox/research/wandb_running_code/Open-BIo-Link/wandb_settings.sh
 #export WANDB_DIR="./wandb_dir/"
 #export TMPDIR="./tmp_dir/"
 #export PYTHONUNBUFFERED=1
@@ -26,4 +26,4 @@
 cd src
 export PYTHONPATH=`pwd`:$PYTHONPATH
 cd ..
-wandb agent rajarshd/pr-cbr/21j6vk1m
+wandb agent rajarshd/pr-cbr/pd8wl0r8
